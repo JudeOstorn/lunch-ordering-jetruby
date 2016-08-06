2c0f15aea125bb9f3b32f2c877fc60f7948500b1
JudeOstorn <judeostorn66@gmail.com>
2016-08-06 21:04:47 +0500

    update config, db

--- db/migrate/20160803150743_create_line_items.rb
+++ db/migrate/20160803150743_create_line_items.rb
@@ -1,8 +1,8 @@
 class CreateLineItems < ActiveRecord::Migration[5.0]
   def change
     create_table :line_items do |t|
-      t.references :menu, index: true, foreign_key: true
-      t.belongs_to :cart, index: true, foreign_key: true
+      t.references :menu, index: true, foreign_key: true, unique: true
+      t.belongs_to :cart, index: true, foreign_key: true, unique: true
 
       t.timestamps null: false
     end
